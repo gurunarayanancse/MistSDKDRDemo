5524297244a1ed15da8c6f941248877dcb456fb7
Guru Narayanan <gurunarayanancse@gmail.com>
2023-04-02 22:53:50 +0530

    Added Map and positioned a userIcon on the map in landscape mode

---
+++ MistSDKDRSampleApp/MistSDKDR-poc/Helpers/Utility.swift
@@ -0,0 +1,12 @@
+//
+//  Utility.swift
+//  MistSDKDRSampleApp
+//
+//  Created by Guru Narayanan on 31/03/23.
+//
+
+import Foundation
+
+class Utility {
+   
+}
---
+++ MistSDKDRSampleApp/MistSDKDR-poc/Model/BeaconRelativePosition.swift
@@ -0,0 +1,13 @@
+//
+//  BeaconRelativePosition.swift
+//  MistSDKDRSampleApp
+//
+//  Created by Guru Narayanan on 31/03/23.
+//
+
+import Foundation
+
+struct BeaconRelativePosition{
+    var relativeX : CGFloat
+    var relativeY : CGFloat
+}
